73b05c0c99db673a986940dc2c115ec957cba7d4
Jt Miclat <jtcmic@gmail.com>
2019-06-21 18:54:54 +0800

    Add precommit hooks

--- run.py
+++ run.py
@@ -4,10 +4,11 @@ import warnings
 
 from flask_shorten import create_app
 
-logging.basicConfig(stream=sys.stdout)
 # Ignore deprecation Warnings after app has been initialize
 # to not pollute the logs when running
-app = create_app()
 warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+logging.basicConfig(stream=sys.stdout)
+app = create_app()
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
