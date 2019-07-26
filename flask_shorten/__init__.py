0e12416e4d81ff5044ebfb2092145dac0a3376fb
Jt Miclat <jtmiclat@gmail.com>
2019-07-26 04:54:47 +0000

    Added talisman when not testing

--- flask_shorten/__init__.py
+++ flask_shorten/__init__.py
@@ -2,6 +2,8 @@ import os
 import logging
 
 from flask import Flask
+from flask_talisman import Talisman
+
 
 from .models import db
 
@@ -14,7 +16,7 @@ logger = logging.getLogger(__name__)
 def set_config(app, testing=False):
     """Set up config
 
-    UsTODO: Move this to an config class
+    TODO: Move this to an config class
     """
     postgres_password = os.getenv("POSTGRES_PASSWORD")
     postgres_user = os.getenv("POSTGRES_USER")
@@ -61,6 +63,8 @@ def create_app(testing=False):
         logger.warning("Found no sentry_dsn. Not using sentry")
     app = Flask(__name__)
     set_config(app, testing)
+    if not app.config["TESTING"]:
+        Talisman(app)
     db.init_app(app)
     from .views import Redirection, HomePage, api, url_bp
 
